6b0a38c5121befcb7fe3f152be6cfcb2a0542d2e
Korootak <jordanwidmer78@gmail.com>
2022-09-01 23:52:05 +0200

    Optimisation du code. Problème rencontré, Les messages de validation ne s'affichaient pas de façon instantanée car l'envoient se fessait en une seule validation. Dorenavent ont à plusieurs Fonction et plusieurs validations

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,72 +25,87 @@ function closeModel() {
   modalbg.style.display = "none";
 }
 
+// attaching foucout event to the dom elements it will be fired when user leaves the text field.
+// it will call the validator method that will validate.
+document.getElementById("first").addEventListener("focusout", validateFirstName)
+document.getElementById("last").addEventListener("focusout", validateLastName)
+document.getElementById("email").addEventListener("focusout", validateEmail);
+document.getElementById("birthdate").addEventListener("focusout", validateBirthDate);
+document.getElementById("quantity").addEventListener("focusout", validateQuantity);
+document.getElementsByName("location").forEach((input) => {
+  input.addEventListener('change', validateLocations);
+});
+
+document.getElementById("checkbox1").addEventListener('change', () => validateCheckBoxes)
+document.getElementById("checkbox2").addEventListener('change', validateCheckBoxes)
+
+
 // function that get called when the form submitted. by default when a
 // form is submitted in javascript,
 // it passes an event object in it in our case i named it as e
 
 // this objects contains bunch of properties and function and I'm using preventDefault function that is used to
 // control the behaviour of form submission.
-
+let errors = [];
 function onFormSubmit(e) {
   e.preventDefault();
-
+  errors = []
   // Validate function returns true if it contains no error otherwise it will return true if there is any error
   if (ValidateForm()) {
     // if the form have no error we are closing the form and showing the message to the user.
     document.getElementById("reserve").style.display = "none";
     document.getElementById("sucessmessage").style.display = "flex";
   } else {
+    console.log(errors)
     console.log("error");
     document.getElementById("sucessmessage").style.display = "none";
   }
 }
 
-function ValidateForm() {
-  // removeAllPreviousErrors function will remove all the previous error messages if we have any.
-  removeAllPreviousErrors();
 
-  // this errors array will contains the records of the errors in the form.
+// getting the values of each form fields
 
-  const errors = [];
 
-  // getting the values of each form fields
+// This Function will validate the firstname textfield
+function validateFirstName() {
   const first = document.getElementById("first").value;
-  const last = document.getElementById("last").value;
-  const email = document.getElementById("email").value;
-  const birthdate = document.getElementById("birthdate").value;
-  const quantity = document.getElementById("quantity").value;
-  const checkbox1 = document.getElementById("checkbox1");
-  const checkbox2 = document.getElementById("checkbox2");
 
-  // Checking erros for each field if error showing error message on the webpage and pusing to the array
-  if (first === "") {
+  if (first === "" || first.length < 2) {
     const el = document.getElementById("firstError");
     el.innerText =
       "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     errors.push({
       error: "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
     });
+    el.classList.remove("is-valid")
     el.classList.add("is-invalid");
   } else {
     const el = document.getElementById("firstError");
     el.innerText = "Joli prénom";
     el.classList.add("is-valid");
   }
-  // Checking erros for each field if error showing error message on the webpage and pusing to the array
-  if (last === "") {
+}
+// This Function will validate the lastname textfield
+function validateLastName() {
+  const last = document.getElementById("last").value;
+  if (last === "" || last.length < 2) {
     const el = document.getElementById("lastError");
     el.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     errors.push({
       error: "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
     });
+    el.classList.remove("is-valid")
     el.classList.add("is-invalid");
   } else {
     const el = document.getElementById("lastError");
     el.innerText = "Un nom original";
     el.classList.add("is-valid");
   }
-  // Checking erros for each field if error showing error message on the webpage and pusing to the array
+}
+
+// This Function will validate the email textfield
+function validateEmail() {
+  const email = document.getElementById("email").value;
   if (
     !email.match(
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -98,6 +113,7 @@ function ValidateForm() {
   ) {
     const el = document.getElementById("emailError");
     el.innerText = "Veuillez saisir une adresse e-mail valide";
+    el.classList.remove("is-valid")
     el.classList.add("is-invalid");
     errors.push({ error: "Veuillez saisir une adresse e-mail valide" });
   } else {
@@ -105,10 +121,14 @@ function ValidateForm() {
     el.innerText = "Merci pour l'email";
     el.classList.add("is-valid");
   }
-  // Checking erros for each field if error showing error message on the webpage and pusing to the array
+}
+// This Function will validate the dateofbirth textfield
+function validateBirthDate() {
+  const birthdate = document.getElementById("birthdate").value;
   if (birthdate === "") {
     const el = document.getElementById("birthDateError");
     el.innerText = "Vous devez entrer votre date de naissance.";
+    el.classList.remove("is-valid")
     el.classList.add("is-invalid");
     errors.push({ error: "Vous devez entrer votre date de naissance." });
   } else {
@@ -116,10 +136,16 @@ function ValidateForm() {
     el.innerText = "Merveilleux";
     el.classList.add("is-valid");
   }
-  // Checking erros for each field if error showing error message on the webpage and pusing to the array
+}
+
+// This Function will validate the quantity textfield
+function validateQuantity() {
+  const quantity = document.getElementById("quantity").value;
+  console.log(quantity)
   if (quantity === "") {
     const el = document.getElementById("quantityError");
     el.innerText = "Veuillez saisir un chiffre";
+    el.classList.remove("is-valid")
     el.classList.add("is-invalid");
     errors.push({ error: "Veuillez saisir un chiffre" });
   } else {
@@ -127,10 +153,14 @@ function ValidateForm() {
     el.innerText = "Le monsieur est un Gamer";
     el.classList.add("is-valid");
   }
-  // Checking erros for each field if error showing error message on the webpage and pusing to the array
+}
+
+// this function will validated the radio buttons in the page
+function validateLocations() {
   if (validateLocation() == -1) {
     const el = document.getElementById("locationError");
     el.innerText = "Please select a location";
+    el.classList.remove("is-valid")
     el.classList.add("is-invalid");
     errors.push({ error: "please select location" });
   } else {
@@ -138,15 +168,44 @@ function ValidateForm() {
     el.innerText = "Ce sont tous de belles ville... m'enfin presque ^^'";
     el.classList.add("is-valid");
   }
+}
+
+// this method will validate the checkboxes
+function validateCheckBoxes() {
+  const checkbox1 = document.getElementById("checkbox1");
+  const checkbox2 = document.getElementById("checkbox2");
+
   if (!checkbox1.checked && !checkbox2.checked) {
     const el = document.getElementById("checkboxError");
     el.innerText =
       "Vous devez vérifier que vous acceptez les termes et conditions.";
+    el.classList.remove("is-valid")
     el.classList.add("is-invalid");
     errors.push({
       error: "Vous devez vérifier que vous acceptez les termes et conditions.",
     });
   }
+  else if (checkbox1.checked || checkbox2.checked) {
+    const el = document.getElementById("checkboxError");
+    el.innerText =
+      "all good";
+    el.classList.remove("is-invalid")
+    el.classList.add("is-valid");
+  }
+}
+
+
+function ValidateForm() {
+  removeAllPreviousErrors();
+
+  validateFirstName();
+  validateLastName();
+  validateEmail();
+  validateBirthDate();
+  validateQuantity();
+  validateLocations();
+  validateCheckBoxes();
+
   return errors.length > 0 ? false : true;
 }
 
